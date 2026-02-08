e0bc0737e9773de77102c4a140cc92b64e265c59
synyee <synyeetan0114@gmail.com>
2026-02-08 13:23:26 +0800

    Update mock_context.js

--- integration/mock_context.js
+++ integration/mock_context.js
@@ -1,6 +1,10 @@
 export const mockContext = {
   keyword: "thirsty",
   objects: ["cup"],
-  location: "hospital",
-  time: "night"
+  time_context: "night",
+  location_context: "hospital",
+  confidence: {
+    keyword: 0.95,
+    object: 0.88
+  }
 };
